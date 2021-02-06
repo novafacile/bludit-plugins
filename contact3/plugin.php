f88aede01b76585d8bdad98a86f767e3c4ad881c
David Donath <43939617+novafacile@users.noreply.github.com>
2021-02-06 14:21:59 +0100

    release Contact3 v2.1.0

--- contact3/plugin.php
+++ contact3/plugin.php
@@ -8,9 +8,9 @@
  *  @author     novafacile OÜ
  *  @copyright  2021 by novafacile OÜ
  *  @license    MIT
- *  @version    2.1.0-beta1
+ *  @version    2.1.0
  *  @see        https://github.com/novafacile/bludit-plugins
- *  @release    2021-02-03
+ *  @release    2021-02-06
  *  @notes      idea based on https://github.com/Fred89/bludit-plugins/tree/master/contact
  *  This program is distributed in the hope that it will be useful - WITHOUT ANY WARRANTY.
  *
